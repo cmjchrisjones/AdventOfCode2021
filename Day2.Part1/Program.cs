0079743e7c05cf5ae19daae8c0f102a1fa5d5090
Chris Jones <cmj.chris.jones@googlemail.com>
2021-12-02 10:20:47 +0000

    Day 2 Part 1

---
+++ Day2.Part1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+var timeStart = Stopwatch.StartNew();
+var filePath = Path.Combine(Directory.GetCurrentDirectory(), "input.data");
+int horizontal, depth = 0;
+
+var rawData = File.ReadAllLines(filePath);
+
+List<KeyValuePair<string, int>> directions = new();
+
+rawData.ToList().ForEach(
+        x =>
+        {
+            string[]? direction = x.Split(' ');
+            directions.Add(new KeyValuePair<string, int>(direction[0], int.Parse(direction[1])));
+        });
+
+var forward = directions.Where(x => x.Key == "forward").Select(_=>_.Value).Sum();
+var dive = directions.Where(x => x.Key == "down").Select(_ => _.Value).Sum();
+var rise = directions.Where(x => x.Key == "up").Select(_ => _.Value).Sum();
+
+horizontal = forward;
+depth = dive - rise;
+
+var position = horizontal * depth;
+
+Console.WriteLine($"Position is {position}");
+
+timeStart.Stop();
+Console.WriteLine($"Elapsed time: {timeStart.Elapsed}");
