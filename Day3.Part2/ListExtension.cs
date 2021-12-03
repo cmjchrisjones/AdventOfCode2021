bb78fb7280092b077649d9e2d367706f93e7ccf0
Chris Jones <cmj.chris.jones@googlemail.com>
2021-12-03 21:32:27 +0000

    Day3.Part2 - its started to get messy!

---
+++ Day3.Part2/ListExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Day3.Part2
+{
+    public static class ListHelpers
+    {
+        public static T FindMostCommon<T>(this IEnumerable<T> list)
+        {  
+
+            if(IsEqualAmountOfBinaryBits(ConvertToIntList(list)))
+            {
+                return list.Max();
+            }
+            return list.GroupBy(_ => _).OrderByDescending(_ => _.Count()).Select(_ => _.Key).First();
+        }
+        public static T FindLeastCommon<T>(this IEnumerable<T> list)
+        {
+            if (IsEqualAmountOfBinaryBits(ConvertToIntList(list)))
+            {
+                var x = list.Min();
+                return x;
+            }
+            return list.GroupBy(_ => _).OrderByDescending(_ => _.Count()).Select(_ => _.Key).Last();
+        }
+
+        private static bool IsEqualAmountOfBinaryBits(IEnumerable<int> list) 
+        {
+            var ones = list.Where(x => x == 1).Count();
+            var zeroes = list.Where(x => x == 0).Count();
+
+            if(ones.Equals(zeroes))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static List<int> ConvertToIntList<T>(IEnumerable<T> list)
+        {
+            List<int> result = new();
+            foreach(var item in list)
+            {
+                int.TryParse(item.ToString(), out int converted);
+                result.Add(converted);
+            }
+            return result;
+        }
+    }
+}
---
+++ Day3.Part2/Program.cs
@@ -0,0 +1,66 @@
+﻿using Day3.Part2;
+using System.Diagnostics;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var timeStart = Stopwatch.StartNew();
+var filePath = Path.Combine(Directory.GetCurrentDirectory(), "input.data");
+
+var rawData = File.ReadAllLines(filePath).ToList();
+
+var mostCommon = rawData;
+var leastCommon = rawData;
+
+
+foreach (var item in mostCommon)
+{
+    for (int j = 0; j < item.Length; j++)
+    {
+        var mostCommonDigit = MostCommonAtIndex(mostCommon, j);
+        mostCommon = mostCommon.Where(_ => _.Substring(j, 1) == mostCommonDigit).ToList();
+    }
+}
+
+foreach (var item in leastCommon)
+{
+    for (int j = 0; j < item.Length; j++)
+    {
+        var leastCommonDigit = LeastCommonAtIndex(leastCommon, j);
+        leastCommon = leastCommon.Where(_ => _.Substring(j, 1) == leastCommonDigit).ToList();
+    }
+}
+
+mostCommon.ForEach(_ => Console.WriteLine(_));
+leastCommon.ForEach(_ => Console.WriteLine(_));
+
+var mostCommonAsDecimal = Convert.ToInt32(mostCommon[0], 2);
+var leastCommonAsDecimal = Convert.ToInt32(leastCommon[0], 2);
+
+Console.Write($"Life support rating: {mostCommonAsDecimal* leastCommonAsDecimal}");
+
+Console.Read();
+
+string MostCommonAtIndex(List<string> list, int index)
+{
+    List<string> indexedDigit = new();
+    foreach (var data in list)
+    {
+        var digit = data.Substring(index, 1);
+        indexedDigit.Add(digit);
+    }
+
+    return indexedDigit.FindMostCommon();
+}
+
+
+string LeastCommonAtIndex(List<string> list, int index)
+{
+    List<string> indexedDigit = new();
+    foreach (var data in list)
+    {
+        var digit = data.Substring(index, 1);
+        indexedDigit.Add(digit);
+    }
+
+    return indexedDigit.FindLeastCommon();
+}
